366eb6b05cb412753a5b1f87a2e92fd647e9a1ad
Kawaiwee <mojunsheng@gmail.com>
2025-07-19 11:58:36 +0630

    modifying post-job page

--- app/post-job/page.tsx
+++ app/post-job/page.tsx
@@ -7,19 +7,37 @@ import { postJobSchema, PostJobFormValues } from './postJobSchema'
 import { Input } from '@/components/ui/input'
 import { Textarea } from '@/components/ui/textarea'
 import { Button } from '@/components/ui/button'
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from '@/components/ui/form'
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from '@/components/ui/form'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 export default function PostJobPage() {
   const form = useForm<PostJobFormValues>({
     resolver: zodResolver(postJobSchema),
     defaultValues: {
       title: '',
-      company: '',
+      salary: '',
+      industry: '',
       location: '',
       description: '',
     },
   })
   
+  const [industries, setIndustries] = useState<{ id: string; name: string }[]>([])
+
+useEffect(() => {
+  const fetchIndustries = async () => {
+    try {
+      const res = await axios.get('/api/industry')
+      setIndustries(res.data)
+    } catch (error) {
+      console.error('Error fetching industries', error)
+    }
+  }
+
+  fetchIndustries()
+}, [])
+
 
   const onSubmit = async (data: PostJobFormValues) => {
     console.log('Submitting job:', data)
@@ -60,17 +78,46 @@ export default function PostJobPage() {
 
           <FormField
             control={form.control}
-            name="company"
+            name="salary"
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Company Name</FormLabel>
+                <FormLabel>Salary</FormLabel>
                 <FormControl>
-                  <Input placeholder="Google" {...field} />
+                  <Input placeholder= ".....Kyats" {...field} />
                 </FormControl>
                 <FormMessage />
               </FormItem>
             )}
           />
+
+          <FormField
+  control={form.control}
+  name="industry"
+  render={({ field }) => (
+    <FormItem>
+      <FormLabel>Industry</FormLabel>
+      <Select
+        onValueChange={field.onChange}
+        defaultValue={field.value}
+      >
+        <FormControl>
+          <SelectTrigger>
+            <SelectValue placeholder="Select an industry" />
+          </SelectTrigger>
+        </FormControl>
+        <SelectContent>
+          {industries.map((industry) => (
+            <SelectItem key={industry.id} value={industry.name}>
+              {industry.name}
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+      <FormMessage />
+    </FormItem>
+  )}
+/>
+
 
           <FormField
             control={form.control}
